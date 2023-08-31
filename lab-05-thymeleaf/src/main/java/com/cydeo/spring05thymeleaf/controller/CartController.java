d6eafe90e768ef073ec77449b42194aec76b9fcf
efecagrisen <efecagrisen@yahoo.com>
2023-08-31 21:21:44 +0300

    Initial commit

---
+++ lab-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.cydeo.spring05thymeleaf.controller;
+
+import com.cydeo.spring05thymeleaf.service.impl.CartServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartController {
+
+    @GetMapping("/cart")
+    public String getCart(Model model){
+
+        model.addAttribute("cart", CartServiceImpl.CART);
+
+        return "/cart/show-cart";
+    }
+}
